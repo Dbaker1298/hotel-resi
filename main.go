1766ee36cdf634a362acadf208a8ec08663e6ae6
David Baker <dpbaker1298@gmail.com>
2023-11-22 15:07:45 -0500

    Add flag package to enable use of a flag for the --listenAddr value

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"flag"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	listenAddr := flag.String("listenAddr", ":5000", "Listen address of the API server")
+	flag.Parse()
+
 	app := fiber.New()
 	apiv1 := app.Group("/api/v1")
 
 	app.Get("/foo", handleFoo)
 	apiv1.Get("/user", handleUser)
-	app.Listen(":5000")
+	app.Listen(*listenAddr)
 }
 
 func handleFoo(c *fiber.Ctx) error {
