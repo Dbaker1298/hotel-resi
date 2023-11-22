436d26c69d99e6e335e425f09a99182bb59d09da
David Baker <dpbaker1298@gmail.com>
2023-11-22 15:04:02 -0500

    Create a Group so that we can version our API. Test with localhost:5000/api/v1/user

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import "github.com/gofiber/fiber/v2"
 
 func main() {
 	app := fiber.New()
+	apiv1 := app.Group("/api/v1")
+
 	app.Get("/foo", handleFoo)
+	apiv1.Get("/user", handleUser)
 	app.Listen(":5000")
 }
 
 func handleFoo(c *fiber.Ctx) error {
 	return c.JSON(map[string]string{"msg": "working just fine"})
+}
+
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"user": "David Baker"})
 }
