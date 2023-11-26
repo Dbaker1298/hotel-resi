393cb1ccb3da1a280bada6fea826cfcf243d2517
David Baker <dpbaker1298@gmail.com>
2023-11-25 21:50:47 -0500

    Refactor API handlers and database stores

--- api/hotel_handler.go
+++ api/hotel_handler.go
@@ -35,12 +35,8 @@ func (h *HotelHandler) HandleGetRooms(c *fiber.Ctx) error {
 
 func (h *HotelHandler) HandleGetHotel(c *fiber.Ctx) error {
 	id := c.Params("id")
-	oid, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return ErrInvalidID()
-	}
 
-	hotel, err := h.store.Hotel.GetHotelByID(c.Context(), oid)
+	hotel, err := h.store.Hotel.GetHotelByID(c.Context(), id)
 	if err != nil {
 		return ErrResourceNotFound("hotel")
 	}
--- main.go
+++ main.go
@@ -57,11 +57,11 @@ func main() {
 
 	// Versioned API routes
 	// user handlers
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 	apiv1.Put("/user/:id", userHandler.HandlePutUser)
 	apiv1.Delete("/user/:id", userHandler.HandleDeleteUser)
 	apiv1.Post("/user", userHandler.HandlePostUser)
 	apiv1.Get("/user", userHandler.HandleGetUsers)
-	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
 	// hotel handlers
 	apiv1.Get("/hotel", hotelHandler.HandleGetHotels)
