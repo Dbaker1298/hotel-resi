7a72e1e9d35f4be92623f443356a050b811c26c8
David Baker <dpbaker1298@gmail.com>
2023-12-03 16:13:14 -0500

    goTagAdd json from nvim

--- types/booking.go
+++ types/booking.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Booking struct {
-	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	UserID     primitive.ObjectID `json:"userId,omitempty" bson:"userId,omitempty"`
-	RoomID     primitive.ObjectID `json:"roomId,omitempty" bson:"roomId,omitempty"`
+	ID         primitive.ObjectID `json:"id,omitempty"         bson:"_id,omitempty"`
+	UserID     primitive.ObjectID `json:"userId,omitempty"     bson:"userId,omitempty"`
+	RoomID     primitive.ObjectID `json:"roomId,omitempty"     bson:"roomId,omitempty"`
 	NumPersons int                `json:"numPersons,omitempty" bson:"numPersons,omitempty"`
-	FromDate   time.Time          `json:"fromDate,omitempty" bson:"fromDate,omitempty"`
-	TillDate   time.Time          `json:"tillDate,omitempty" bson:"tillDate,omitempty"`
-	Canceled   bool               `json:"canceled" bson:"canceled"`
+	FromDate   time.Time          `json:"fromDate,omitempty"   bson:"fromDate,omitempty"`
+	TillDate   time.Time          `json:"tillDate,omitempty"   bson:"tillDate,omitempty"`
+	Canceled   bool               `json:"canceled"             bson:"canceled"`
 }
